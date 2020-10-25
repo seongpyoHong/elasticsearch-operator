54b8b7d715da931573ebc19fbbe4db446b4756ea
Seongpyo Hong <sphong0417@gmail.com>
2020-10-25 15:12:25 +0900

    Add Controller for Elasticsearch CR

--- controllers/elasticsearch_controller.go
+++ controllers/elasticsearch_controller.go
@@ -18,6 +18,10 @@ package controllers
 
 import (
 	"context"
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/api/batch/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -38,16 +42,43 @@ type ElasticsearchReconciler struct {
 // +kubebuilder:rbac:groups=sphong.com.my.domain,resources=elasticsearches/status,verbs=get;update;patch
 
 func (r *ElasticsearchReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("elasticsearch", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("elasticsearch", req.NamespacedName)
 
-	// your logic here
+	//Fetch the Elasticsearch Cluster
+	elasticsearch := &sphongcomv1alpha1.Elasticsearch{}
+	err := r.Get(ctx, req.NamespacedName, elasticsearch)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("Elasticsearch Resource Not Found!. Ignore since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get Elasticsearch")
+		return ctrl.Result{}, err
+	}
 
+	//TODO : check already exist => ensure the size is the same as spec => update status
+	// 1. Master Node
+	// 2. Client Node
+	// 3. Hot Data Node
+	// 4. Wram Data Node
+	// 5. Cerebro
+	// 6. Kibana
+	// 7. Curator
 	return ctrl.Result{}, nil
 }
 
 func (r *ElasticsearchReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&sphongcomv1alpha1.Elasticsearch{}).
-		Complete(r)
+		Owns(&v1.Deployment{}).
+		Owns(&v1.StatefulSet{}).
+		Owns(&v1beta1.CronJob{}).
+		WithOptions(controller.Options{
+			MaxConcurrentReconciles: 3,
+		}).Complete(r)
 }
